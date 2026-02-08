bcacfcc4e982e726071f7e2b5bc487e4382697dc
unknown <hexCoder20@users.noreply.github.com>
2026-02-08 14:20:27 +0500

    final product

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,35 @@ import './components/Hero.css';
 
 function App() {
   const projects = [
-    {
-      id: 1,
-      title: "E-Commerce Store",
-      desc: "A full shop built with React and FakeStore API.",
-      tech: "React, CSS"
-    }, {
-      id: 2,
-      title: "Task Manager",
-      desc: "Productivity app to track daily tasks.",
-      tech: "JavaScript, LocalStorage"
-    }, {
-      id: 3,
-      title: "Travel Dashboard",
-      desc: "Interactive map and booking interface.",
-      tech: "React Router, API"
-    }, {
-      id: 4,
-      title: "Crypto Tracker",
-      desc: "Live prices of top cryptocurrencies.",
-      tech: "API, Charts"
-    }
-  ]
+  { 
+    id: 1, 
+    title: "E-Commerce Store", 
+    desc: "A full shop built with React and FakeStore API.",
+    tech: "React, CSS",
+    image: "https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  },
+  { 
+    id: 2, 
+    title: "Task Manager", 
+    desc: "Productivity app to track daily tasks.",
+    tech: "JavaScript, LocalStorage",
+    image: "https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  },
+  { 
+    id: 3, 
+    title: "Travel Dashboard", 
+    desc: "Interactive map and booking interface.",
+    tech: "React Router, API",
+    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  },
+  { 
+    id: 4, 
+    title: "Crypto Tracker", 
+    desc: "Live prices of top cryptocurrencies.",
+    tech: "API, Charts",
+    image: "https://images.unsplash.com/photo-1621504450168-b8c437536968?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+  }
+];
 
   return (
     <div className="app">
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function ProjectCard({project}){
     return(
         <div className="card">
+            <img src={project.image} alt={project.title} className='card-img'></img>
             <h3>{project.title}</h3>
             <p>{project.desc}</p>
             <div className="tech-stack">{project.tech}</div>
